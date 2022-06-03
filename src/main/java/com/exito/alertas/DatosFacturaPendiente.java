cf92bea30e3155b3825479061d5012f6b16403ab
cesar <cesare.munoz@gmail.com>
2022-06-03 09:43:30 -0500

    se crea la tabla con los datos quemados

---
+++ src/main/java/com/exito/alertas/DatosFacturaPendiente.java
@@ -0,0 +1,11 @@
+package com.exito.alertas;
+
+import lombok.Data;
+
+@Data
+public class DatosFacturaPendiente {
+	private String invoiceStatus;
+	private String cedula;
+	private String tipoDependencia;
+
+}
