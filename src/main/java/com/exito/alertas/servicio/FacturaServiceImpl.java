fe681ad00e96b2eb36abf41eb0a592d9937e5681
cesar <cesare.munoz@gmail.com>
2022-06-07 13:00:46 -0500

    Se creó la capa de servicio y se agregó la notación transactional

--- src/main/java/com/exito/alertas/servicio/FacturaServiceImpl.java
+++ src/main/java/com/exito/alertas/servicio/FacturaServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.exito.alertas.Factura;
 import com.exito.alertas.dao.FacturaDao;
@@ -15,11 +16,13 @@ public class FacturaServiceImpl implements FacturaService{
 	private FacturaDao facturaDao;
 
 	@Override
+	@Transactional(readOnly=true)
 	public List<Factura> listarFacturas() {		
 		return (List<Factura>)facturaDao.findAll();
 	}
 
 	@Override
+	@Transactional(readOnly=true)
 	public Factura encontrarFactura(Factura factura) {		
 		return facturaDao.findById(factura.getOrderId()).orElse(null);
 	}
